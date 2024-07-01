9379a5459ca09a911fb0452de55dd8f6211417a4
ReallyBadDeveloper <reallybaddeveloper@gmail.com>
2024-07-01 09:08:38 -0400

    test

--- index.js
+++ index.js
@@ -1,82 +1,101 @@
-const express = require('express');
-const ipban = require('./ipban.js');
-const proxy = require('express-http-proxy');
-const app = express();
-const dotenv = require('dotenv').config();
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
-const port = 443;
+const express = require('express')
+const sudoprompt = require('sudo-prompt')
+const ipban = require('./ipban.js')
+const proxy = require('express-http-proxy')
+const app = express()
+const dotenv = require('dotenv').config()
+const https = require('https')
+const fs = require('fs')
+const path = require('path')
+const { stdout } = require('process')
+const port = 443
 
 const cts = {
-    cert: fs.readFileSync(process.env.CERT_DIR),
-    key: fs.readFileSync(process.env.KEY_DIR)
+	cert: null,
+	key: null,
 }
 
-ipban.init();
+sudoprompt.exec('cat ' + process.env.CERT_DIR, (stdout,stderr) => {
+    if (stderr) throw stderr;
+    cts.cert = stdout;
+})
 
-app.use(express.json());
+sudoprompt.exec('cat ' + process.env.KEY_DIR, (stdout,stderr) => {
+    if (stderr) throw stderr;
+    cts.key = stdout;
+})
+
+ipban.init()
+
+app.use(express.json())
 app.use((req, res, next) => {
-    if (ipban.checkIP(ipban.getIP(req)) === true) {
-        res.status(401).json(JSON.stringify({
-            warning: "You have been IP banned."
-        }));
-        return;
-    }
-    next();
-});
+	if (ipban.checkIP(ipban.getIP(req)) === true) {
+		res.status(401).json(
+			JSON.stringify({
+				warning: 'You have been IP banned.',
+			})
+		)
+		return
+	}
+	next()
+})
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static('public'))
 
-app.get('/', (req,res) => {
-    if (ipban.checkIP(ipban.getIP(req)) === true) {
-        res.status(401).send(JSON.stringify({
-            warning: "You have been IP banned."
-        }))
-    }
-    res.sendFile(__dirname + `/index.html`);
-});
+app.get('/', (req, res) => {
+	if (ipban.checkIP(ipban.getIP(req)) === true) {
+		res.status(401).send(
+			JSON.stringify({
+				warning: 'You have been IP banned.',
+			})
+		)
+	}
+	res.sendFile(__dirname + `/index.html`)
+})
 
-app.get('/contact', (req,res) => {
-    if (ipban.checkIP(ipban.getIP(req)) === true) {
-        res.status(401).send(JSON.stringify({
-            warning: "You have been IP banned."
-        }))
-    }
-    res.sendFile(__dirname + `/contact.html`);
-});
+app.get('/contact', (req, res) => {
+	if (ipban.checkIP(ipban.getIP(req)) === true) {
+		res.status(401).send(
+			JSON.stringify({
+				warning: 'You have been IP banned.',
+			})
+		)
+	}
+	res.sendFile(__dirname + `/contact.html`)
+})
 
-app.post('/contactsubmit', (req,res) => {
-    var body = req.body;
-    if (typeof req.body.name !== 'string') {
-        ipban.ban(ipban.getIP(req));
-        res.status(401).send(JSON.stringify({
-            warning: "You have been IP banned."
-        }));
-        return;
-    }
-    fetch(process.env.WEBHOOK, {
-        method: 'post',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            embeds: [
-                {
-                    title: `Message from ${body.name} (${body.email})`,
-                    description: `**Message:**\n${'```'}${body.msg}${'```'}`,
-                    color: 16540159,
+app.post('/contactsubmit', (req, res) => {
+	var body = req.body
+	if (typeof req.body.name !== 'string') {
+		ipban.ban(ipban.getIP(req))
+		res.status(401).send(
+			JSON.stringify({
+				warning: 'You have been IP banned.',
+			})
+		)
+		return
+	}
+	fetch(process.env.WEBHOOK, {
+		method: 'post',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			embeds: [
+				{
+					title: `Message from ${body.name} (${body.email})`,
+					description: `**Message:**\n${'```'}${body.msg}${'```'}`,
+					color: 16540159,
+				},
+			],
+		}),
+	})
+	res.status(200)
+})
 
-                }
-            ]
-        })
-    })
-    res.status(200);
-});
-
-app.use((req,res,next) => {
-    res.sendFile(__dirname + '/404.html');
-});
+app.use((req, res, next) => {
+	res.sendFile(__dirname + '/404.html')
+})
 
 https.createServer(cts, app).listen(port)
